102d790990923af111742095cef125e7a1ea6572
bananabread08 <rmkfermin@gmail.com>
2021-03-23 18:48:26 +0800

    initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const getWeatherData = async () => {\n  try {\n    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=568d16f8e8cf2516ecd7cf2bd2689610', { mode: 'cors' });\n    const weatherData = await response.json();\n    console.log(weatherData);\n  } catch (error) { // don't mind this errorfor now\n    console.log(error);\n  }\n};\n\ngetWeatherData();\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;

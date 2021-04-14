e66fe6c3be4067b2fed72ebb08734f6a713bc19a
bananabread08 <rmkfermin@gmail.com>
2021-04-14 14:44:18 +0800

    fixed mobile UI, removed console.logs

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,7 +2,6 @@ const dom = (() => {
   const input = document.querySelector('#search-item');
   const form = document.querySelector('form');
   const locationEl = document.querySelector('#location');
-  // const weatherEl = document.querySelector('#weather');
   const descriptionEl = document.querySelector('#description');
   const temperatureEl = document.querySelector('#temperature');
   const humidityEl = document.querySelector('#humidity');
@@ -14,7 +13,6 @@ const dom = (() => {
     input,
     form,
     locationEl,
-    // weatherEl,
     descriptionEl,
     temperatureEl,
     humidityEl,

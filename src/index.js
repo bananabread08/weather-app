3fd5432f30cce5a3bca8a290731c482ddb42d51a
bananabread08 <rmkfermin@gmail.com>
2021-12-10 17:06:13 +0800

    consolidated all apiFunctions to one file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import getWeatherData from './modules/getweatherdata';
+import * as apiFxns from './modules/apiFunctions';
 //import dom from './modules/dom';
 import './styles/style.css';
 import './assets/background.png';
 
-getWeatherData('Philippines');
+const form = document.querySelector('form');
+const input = document.querySelector('#search-item');
+
+async function getWeatherData(location) {
+  try {
+    const main_data = await apiFxns.getCoords(location);
+    console.log(main_data);
+    const forecast = await apiFxns.getForecast(
+      main_data.coord.lon,
+      main_data.coord.lat
+    );
+    console.log(forecast);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeatherData('Batac');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWeatherData(input.value);
+  form.reset();
+});
--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -4,18 +4,17 @@ async function getForecast(lon, lat) {
   try {
     const API_KEY = retrieveApiKey();
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${API_KEY}`,
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}`,
       { mode: 'cors' }
     );
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
-    //dom.errorEl.style.visibility = 'visible';
   }
 }
 
-export default async function getWeatherData(location) {
+async function getCoords(location) {
   try {
     const API_KEY = retrieveApiKey();
     const response = await fetch(
@@ -23,10 +22,15 @@ export default async function getWeatherData(location) {
       { mode: 'cors' }
     );
     const data = await response.json();
-    const { lon, lat } = data.coord;
-    getForecast(lon, lat);
+    return {
+      coord: data.coord,
+      city: data.name,
+      country: data.sys.country,
+    };
   } catch (error) {
     console.log(error);
     //dom.errorEl.style.visibility = 'visible';
   }
 }
+
+export { getCoords, getForecast };

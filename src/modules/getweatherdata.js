9e4c84166fb5514dd525eb8d362543d0a04b9099
bananabread08 <rmkfermin@gmail.com>
2021-12-10 16:18:23 +0800

    implemented a working data fetcher using 2 API calls

--- src/modules/getweatherdata.js
+++ src/modules/getweatherdata.js
@@ -1,6 +1,19 @@
 import retrieveApiKey from './apikey';
-//import displayController from './displaycontroller';
-//import dom from './dom';
+
+async function getForecast(lon, lat) {
+  try {
+    const API_KEY = retrieveApiKey();
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${API_KEY}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    //dom.errorEl.style.visibility = 'visible';
+  }
+}
 
 export default async function getWeatherData(location) {
   try {
@@ -10,17 +23,8 @@ export default async function getWeatherData(location) {
       { mode: 'cors' }
     );
     const data = await response.json();
-    console.log(data);
-    // console.log(data.sys.country);
-    const pickedData = {
-      location: data.name,
-      countryCode: data.sys.country,
-      weather: data.weather[0].main,
-      description: data.weather[0].description,
-      temperature: data.main.temp,
-      humidity: data.main.humidity,
-    };
-    //displayController(pickedData);
+    const { lon, lat } = data.coord;
+    getForecast(lon, lat);
   } catch (error) {
     console.log(error);
     //dom.errorEl.style.visibility = 'visible';

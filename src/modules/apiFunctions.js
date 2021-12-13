181e1890bc8c2b0d098b0287352f8729bc5304f5
bananabread08 <rmkfermin@gmail.com>
2021-12-13 10:23:18 +0800

    added spinner for loading weathers

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -2,6 +2,8 @@ const API_KEY = '568d16f8e8cf2516ecd7cf2bd2689610';
 
 async function getForecast(lon, lat) {
   try {
+    let loader = `<i class="fas fa-snowflake fa-spin fa-10x loader"></i>`;
+    document.querySelector('.forecast').innerHTML = loader;
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}`,
       { mode: 'cors' }
@@ -15,6 +17,8 @@ async function getForecast(lon, lat) {
 
 async function getCoords(location) {
   try {
+    let loader = `<i class="fas fa-snowflake fa-spin fa-10x loader"></i>`;
+    document.querySelector('.data-container').innerHTML = loader;
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`,
       { mode: 'cors' }

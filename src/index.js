1adbc3825cb62e7c312d6464baa982d68ed9895c
bananabread08 <rmkfermin@gmail.com>
2021-12-14 09:28:46 +0800

    fixed loaders not in sync, removed html comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as apiFxns from './modules/apiFunctions';
-import updateDom from './modules/updateUI';
+import updateUI from './modules/updateUI';
 import './styles/style.css';
 import './assets/background.png';
 
@@ -9,6 +9,9 @@ const errorEl = document.querySelector('.error');
 
 async function getWeatherData(location) {
   try {
+    let loader = `<i class="fas fa-snowflake fa-spin fa-10x loader"></i>`;
+    document.querySelector('.forecast').innerHTML = loader;
+    document.querySelector('.data-container').innerHTML = loader;
     const main_data = await apiFxns.getCoords(location);
     console.log(main_data);
     const forecast = await apiFxns.getForecast(
@@ -16,7 +19,7 @@ async function getWeatherData(location) {
       main_data.coord.lat
     );
     console.log(forecast);
-    updateDom(main_data, forecast, errorEl);
+    updateUI(main_data, forecast, errorEl);
   } catch (error) {
     errorEl.style.visibility = 'visible';
     console.log(error);
